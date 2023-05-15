2ddfd524c70514ee8c074452073765c4b4d83ec7
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-05-15 10:32:16 +0200

    pages templates

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,8 @@
-import styles from "@styles/layout.module.css";
-
 export default function Footer() {
-  return <footer className={styles.footer}>Footer goes here</footer>;
+  return (
+    <footer className="bg-thritery h-16 flex text-secondary-400 justify-center items-center">
+      copyright © 2023 All rights Reserved for{" "}
+      <span className="pl-3 tracking-widest font-bold">PARICHEHR MOHEBBI</span>
+    </footer>
+  );
 }
